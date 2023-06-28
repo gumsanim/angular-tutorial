95c9f259b5c3adcc9c3dfdf345af18c5ef35fbc3
jaeho <gumsanim1993@gmail.com>
2023-06-28 22:53:54 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp2';
+  newMemberName = '';
+  members: string[] = [];
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
